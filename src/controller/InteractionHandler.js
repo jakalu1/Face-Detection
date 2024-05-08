f8ac6fd185c38ee966ff3fb99fb06613f6908afb
jakalu1 <131008073+jakalu1@users.noreply.github.com>
2024-05-08 07:41:25 -0400

    Update InteractionHandler.js

--- src/controller/InteractionHandler.js
+++ src/controller/InteractionHandler.js
@@ -1,1 +1,1 @@
-// would be the controller file that handles interactions btwn model and view but didn't need one for this program
+// would be the controller file that handles interactions btwn src/model and src/view but didn't need one for this program
